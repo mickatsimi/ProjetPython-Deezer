95b318a1fdf69b14ab43cb0b4c9906589a2102c3
mickatsimi <62539302+mickatsimi@users.noreply.github.com>
2022-01-08 10:20:35 +0100

    Update Projet.py

--- Projet.py
+++ Projet.py
@@ -1,103 +1,140 @@
-INSTALLATION DE LA LIBRAIRIE NECESSAIRE AU BON FONCTIONNEMENT DU PROGRAMME
-
-pip install deezer-python
-
-import deezer as dp
-
 """ACCES VIA LES CODES DE CONNEXION A L'API DEEZER"""
 
 client = dp.Client(app_id='mickaeltsimi@yahoo.fr', app_secret='Mickael1')
 
-nom_artiste=input("inserez le nom de l'artiste que vous souhaitez : ")
-artiste=client.get_artist(nom_artiste)
-print(artiste)
-
-"""TRAVAIL SUR ARTISTES"""
-
-albums_de_lartiste=artiste.get_albums()[0].get_tracks()
-print(albums_de_lartiste)
-
-for j in range(len(artiste.get_albums())):
+print("Welcome to deezer-python",
+          "\nLes meilleures playlists vous attendent ici!!")
     
-    cpt=0
-    alb=artiste.get_albums()[j].get_tracks()
-    if len(alb)>1: #no single
-        for elt in range(len(alb)):
-            #print(alb[elt]) 
-            if "feat." in str(alb[elt]):
-                cpt+=1
-        print("nom album: ",artiste.get_albums()[j]," nombre de feats: ",cpt," nombre de sons: ",len(alb))
+bdd=[]
+bdd_playlist=[]
+while True:
 
-nbr=artiste.get_albums()
-for i in range (len(nbr)):
-  somme =0
-  for j in range(len(nbr[i].get_tracks())):
-    if len(nbr[i].get_tracks())<2:
-      print("C'est un single")
+    print("\n Que voulez-vous?",
+      "A - Des infos sur un artiste",
+      "B - Générer une playlist",
+      "C - Voir l'historique de vos recherches",
+      "D - Quitter",
+      sep="\n")
+    action=input()
+    
+    while (action.lower()!="a" and action.lower()!="b" and action.lower()!="c" and action.lower()!="d"):
+        action=input()
+    
+    if (action.lower() == "a"):
+        nom_artiste=input("inserez le nom de l'artiste que vous souhaitez : ")
+        #artiste=client.get_artist(input("inserez le nom de l'artiste que vous souhaitez : "))
+        artiste=Artiste(nom_artiste)
+        bdd.append(artiste.nom)
+        print(artiste.presentation())
+        #print(artiste, "Nombre de projets (singles inclus): ",len(artiste.get_albums()))
+        print("Albums et leurs informations: \n")
+        limite=artiste.nb_projets
+        if (limite > 7):
+            limite=7
+        for j in range(limite):#infos sur les albums de l'artiste
+            cpt=0
+            somme =0
+            alb=artiste.albums[j].get_tracks()
+            if len(alb)>1: #no single
+                for elt in range(len(alb)):
+                    somme=somme+alb[elt].duration 
+                    if "feat." in str(alb[elt]):
+                        cpt+=1
+                print("nom album: ",artiste.albums[j]," nombre de feats: ",cpt," nombre de sons: ",len(alb),
+                      "durée en minutes: ",somme/60,'------------------------',sep="\n")
+                    
+    elif (action.lower() == "d"):
+        break
+    
+    elif (action.lower() == "b"):
+        print("Trouvons ensemble la playlist parfaite pour vous! Faites votre choix:",
+          "a- Playlist à partir d'un artiste",
+          "b - les morcceaux les plus écoutés en ce moment",
+          "c - Playlist découverte à partir du top France",
+          "d - Sorties récentes",
+          sep="\n")
+        pl=input()
+        while (pl!="a" and pl!="b" and pl!="c" and pl!="d"):
+            pl=input()
+            
+        if (pl == "a"):
+            p_a=Playlist(1)
+            a=input('choisissez votre artiste de départ : ')
+            artiste_playlist=details_artiste(a)
+            bdd.append(artiste_playlist.nom)
+            print("Si vous aimez ",artiste_playlist.nom, ", vous aimerez forcément ces morceaux: \n")
+            for j in range(5):
+              print('------------------------')
+              print(artiste_playlist.lien[j].name)
+              print('------------------------')
+              for i in range(3):
+                print(artiste_playlist.lien[j].get_top()[i].title)
+                p_a.add_son(artiste_playlist.lien[j].get_top()[i])#ajout du morceau a la playlist
+                
+            #nommer la playlist
+            nom=input("Donnez un nom à votre playlist: ")
+            p_a.nommer(nom)
+            bdd_playlist.append(p_a.nom)
+        elif (pl == "b"):
+            client2 = dp.Client(app_id='mickaeltsimi@yahoo.fr', app_secret='Mickael1')
+            id=1767932902#top 25 FRANCE
+            p_b=deezer_playlist(id)
+            p_b.generer()
+            for i in range(9):
+              print(p_b.sons[i],"\n")
+            #nommer la playlist
+            nom=input("Donnez un nom à votre playlist: ")
+            p_b.nommer(nom)
+            bdd_playlist.append(p_b.nom)
+            
+        elif (pl == "c"):
+            client2 = dp.Client(app_id='mickaeltsimi@yahoo.fr', app_secret='Mickael1')
+            id=1767932902#top 25 FRANCE
+            p_c=deezer_playlist(id)
+            p_c.generer()
+            p_c.artistes_de_la_playlist()
+            for i in range(3):
+              print(p_c.artistes[i],"\n")
+              for j in range(3):
+                print('------------------------')
+            
+                for k in range(1):
+                  print(p_c.artistes[i].get_related()[j].name,": ", p_c.artistes[i].get_related()[j].get_top()[k].title)
+                print('------------------------')
+            #nommer la playlist
+            nom=input("Donnez un nom à votre playlist: ")
+            p_c.nommer(nom)
+            bdd_playlist.append(p_c.nom)
+        else:
+            print("Ces albums récents pourraient vous plaire; jetez-y un coup d'oeil! \n")
+            client2 = dp.Client(app_id='mickaeltsimi@yahoo.fr', app_secret='Mickael1')
+            id=1767932902#top 25 FRANCE
+            p_d=deezer_playlist(id)
+            p_d.generer()
+            p_d.artistes_de_la_playlist()
+            for i in range(7):
+              print(p_d.artistes[i])
+              print(p_d.artistes[i].get_albums()[0])
+              print(p_d.artistes[i].get_albums()[0].release_date)
+            #nommer la playlist
+            nom=input("Donnez un nom à votre playlist: ")
+            p_d.nommer(nom)
+            bdd_playlist.append(p_d.nom)
+    
     else:
-      
-      somme=somme+nbr[i].get_tracks()[j].duration
-  print("l'album",nbr[i],"dure",(somme/60),"minutes")
-
-a=input('choisissez votre artiste de départ : ')
-artiste=client.get_artist(a)
-for j in artiste.get_related():
-  print('------------------------')
-  print(j.name)
-  print('------------------------')
-  for i in range(5):
-    print(j.get_top()[i].title)
-
-"""**TRAVAIL SUR PLAYLIST **"""
-
-client2 = dp.Client(app_id='mickaeltsimi@yahoo.fr', app_secret='Mickael1')
-
-liste=[]
-id=1767932902
-for i in range(len(client2.get_playlist(id).get_tracks())):
-  liste.append(client2.get_playlist(id).get_tracks()[i].get_artist())
-  print(client2.get_playlist(id).get_tracks()[i].get_artist())
-
-"""CREATION DE PLAYLIST EN LIEN AVEC LES ARTISTES DU TOP France 2021
-
-"""
-
-l=[]
-for i in range(len(client2.get_playlist(id).get_tracks())):
-  print(client2.get_playlist(id).get_tracks()[i].get_artist())
-  print('Playlist Découverte')
-  for j in client2.get_playlist(id).get_tracks()[i].get_artist().get_related():
-    print('------------------------')
-    print(j.name)
-    print('------------------------')
-    for k in range(len(j.get_top())):
-      print(j.get_top()[k].title)
-      
+        if (len(bdd)==0):
+            print("aucun artiste encore recherché. Qu'attendez-vous!?")
+        else:
+            print("Liste des artistes recherchés:")
+            for obj in bdd:
+                print(obj,"\n")
+        
+        if (len(bdd_playlist)==0):
+            print("aucune playlist pour l'instant. commencez dès à présent!")
+        else:
+            print("Vos playlists déja créées:")
+            for obj in bdd_playlist:
+                print(obj,"\n")
     
     
-
-###print(len(client2.get_playlist(id).get_tracks()[i].get_artist().get_related()))
-
-"""TOP 5 DES SONS DES ARTISTES DU TOP 25 FRANCE"""
-
-for i in range(len(client2.get_playlist(id).get_tracks())):
-  print(client2.get_playlist(id).get_tracks()[i].get_artist())
-  print(client2.get_playlist(id).get_tracks()[i].get_artist().get_top())
-
-"""ALBUM LE PLUS Recent DES ARTISTES DU TOP 25"""
-
-for i in range(len(client2.get_playlist(id).get_tracks())):
-  print(client2.get_playlist(id).get_tracks()[i].get_artist())
-  print(client2.get_playlist(id).get_tracks()[i].get_artist().get_albums()[0])
-  print(client2.get_playlist(id).get_tracks()[i].get_artist().get_albums()[0].release_date)
-
-"""GENERATEUR DE PLAYLIST"""
-
-a=input('choisissez votre artiste de départ : ')
-artiste=client.get_artist(a)
-for j in artiste.get_related():
-  print('------------------------')
-  print(j.name)
-  print('------------------------')
-  for i in range(5):
-    print(j.get_top()[i].title)
+    
